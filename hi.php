b6b3730ae161d5fd37322881fd4c9aa921c44225
Naomi Burhoe <Naomi.burhoe@hotmail.com>
2023-01-16 18:05:16 -0500

    Add files via upload

---
+++ hi.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<p>Hello World :)</p>
+		<p>Name entered: <?php echo $_POST["name"]; ?></p>
+		<p>Text entered: <?php echo $_POST["text"]; ?></p>
+		<p>Color picked: <?php echo $_POST["color"]; ?></p>
+		<p>Email entered: <?php echo $_POST["email"]; ?></p>
+		<p>Text in textarea entered: <?php echo $_POST["textarea"]; ?></p>
+		<p>Phone number entered: <?php echo $_POST["phone"]; ?></p>
+		
+	</body>
+</html>
